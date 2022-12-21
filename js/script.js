96c92ada3ca98b8c5f400acfc0a16247c7fb67c1
Serena-Dong <serenadong3159@icloud.com>
2022-12-21 16:24:06 +0100

    stampo in console

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
+// DATI
 const info = {
     name : ['Wayne Barnett', 'Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos'],
     role : ['Founder & CEO', 'Chief Editor', 'Office Manager', 'Social Media Manager', 'Developer', 'Graphic Designer'],
     image : ['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg', 'walter-gordon-office-manager.jpg', 'angela-lopez-social-media-manager.jpg', 'scott-estrada-developer.jpg', 'barbara-ramos-graphic-designer.jpg']
 }
+//Stampo in console
+console.log(info.name[0],info.role[0],info.image[0])
+console.log(info.name[1],info.role[1],info.image[1])
+console.log(info.name[2],info.role[2],info.image[2])
+console.log(info.name[3],info.role[3],info.image[3])
+console.log(info.name[4],info.role[4],info.image[4])
+console.log(info.name[5],info.role[5],info.image[5])
+
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto 
+    */
