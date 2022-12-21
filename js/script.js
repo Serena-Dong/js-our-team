4f8f8c965f7bee1fb984340c0bdba7da375ada1a
Serena-Dong <serenadong3159@icloud.com>
2022-12-21 16:57:00 +0100

    stampa in console con for

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+//ELEMENTI dal DOM
+const pagina = document.getElementById('main-row');
+
 // DATI
 const info = {
     name : ['Wayne Barnett', 'Angela Caroll', 'Walter Gordon', 'Angela Lopez', 'Scott Estrada', 'Barbara Ramos'],
@@ -5,12 +8,14 @@ const info = {
     image : ['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg', 'walter-gordon-office-manager.jpg', 'angela-lopez-social-media-manager.jpg', 'scott-estrada-developer.jpg', 'barbara-ramos-graphic-designer.jpg']
 }
 //Stampo in console
-console.log(info.name[0],info.role[0],info.image[0])
-console.log(info.name[1],info.role[1],info.image[1])
-console.log(info.name[2],info.role[2],info.image[2])
-console.log(info.name[3],info.role[3],info.image[3])
-console.log(info.name[4],info.role[4],info.image[4])
-console.log(info.name[5],info.role[5],info.image[5])
+for (let key in info){
+    console.log(info[key][0])
+    console.log(info[key][1])
+    console.log(info[key][2])
+    console.log(info[key][3])
+    console.log(info[key][4])
+    console.log(info[key][5])
+}
 
 
 /*
@@ -24,4 +29,4 @@ BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto 
-    */
+*/
